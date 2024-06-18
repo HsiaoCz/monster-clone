921938111a09a45acb95f4a90632f70352505980
HsiaoCz <984274788@qq.com>
2024-06-18 21:54:10 +0800

    add rainbow

---
+++ rainbow/app/db/migrations/20240610161057_create_users_table.sql
@@ -0,0 +1,12 @@
+-- +goose Up
+create table if not exists users(
+	id integer primary key,
+	email text unique not null,
+	password_hash text not null,
+	username text not null,
+	email_verified_at timestamp with time zone,
+	created_at timestamp with time zone not null,
+	updated_at timestamp with time zone not null
+);
+-- +goose Down
+drop table if exists users;
