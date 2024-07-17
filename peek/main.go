fea51f7928f70397d20bd3179f8b47ca56501511
HsiaoCz <984274788@qq.com>
2024-07-17 10:50:30 +0800

    add some

--- peek/main.go
+++ peek/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 
+	"github.com/HsiaoCz/monster-clone/peek/handlers"
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
 )
+
+var config = fiber.Config{
+	ErrorHandler: func(c *fiber.Ctx, err error) error {
+		if e, ok := err.(handlers.ErrorMsg); ok {
+			return c.Status(e.Status).JSON(&e)
+		}
+		e := handlers.ErrorMsg{
+			Status:  http.StatusInternalServerError,
+			Message: err.Error(),
+		}
+		return c.Status(e.Status).JSON(&e)
+	},
+}
 
 func main() {
 	if err := godotenv.Load(); err != nil {
@@ -15,8 +32,17 @@ func main() {
 
 	var (
 		port = os.Getenv("PORT")
-		app  = fiber.New()
+		app  = fiber.New(config)
 	)
-
-	app.Listen(port)
+	go func() {
+		if err := app.Listen(port); err != nil {
+			log.Fatal(err)
+		}
+	}()
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+	if err := app.Shutdown(); err != nil {
+		log.Fatal(err)
+	}
 }
