82d26dc7a388e566e7b0f082395995655f12faf6
HsiaoCz <984274788@qq.com>
2024-07-20 17:29:11 +0800

    add some

--- santino/handler/comment_handler_test.go
+++ santino/handler/comment_handler_test.go
@@ -1,5 +1,51 @@
 package handler
 
-import "testing"
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-func TestCreateComment(t *testing.T) {}
+	"github.com/HsiaoCz/monster-clone/santino/data"
+	"github.com/HsiaoCz/monster-clone/santino/db"
+	"github.com/HsiaoCz/monster-clone/santino/types"
+	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
+)
+
+func TestCreateComment(t *testing.T) {
+	if err := godotenv.Load("../.env"); err != nil {
+		t.Fatal(err)
+	}
+	if err := db.Init(); err != nil {
+		t.Fatal(err)
+	}
+
+	userHandler := NewUserHandler(data.NewUserData(db.Get()))
+
+	app := fiber.New()
+
+	app.Post("/user", userHandler.HandleCreateUser)
+
+	parmas := types.CreateUserParams{
+		Username:         "shawcz",
+		Email:            "shawcz@gmail.com",
+		Password:         "shawcz123",
+		Synopsis:         "something wrong",
+		Avatar:           "./picture/1233.jpg",
+		Background_Image: "./bgi/1234.jpg",
+	}
+
+	b, _ := json.Marshal(parmas)
+	req := httptest.NewRequest("POST", "/user", bytes.NewBuffer(b))
+	req.Header.Add("Content-Type", "application/json")
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+	userParams := types.CreateUserParams{}
+
+	json.NewDecoder(resp.Body).Decode(&userParams)
+}
